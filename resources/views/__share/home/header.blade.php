97df621622544a3948972b1d14f52141860c1c64
hovanphuc97 <hovanphuc97t10@gmail.com>
2019-08-06 13:59:49 +0700

    no message

--- resources/views/__share/home/header.blade.php
+++ resources/views/__share/home/header.blade.php
@@ -8,10 +8,18 @@
 
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
-			@foreach($menus as $menu )
-	          <li class="nav-item"><a href="{{route($menu->slug)}}" class="nav-link">{{$menu -> name}}</a></li>
-	        @endforeach
-			</ul>
+	          <li class="nav-item active"><a href="{{ route('/') }}" class="nav-link">Trang chủ</a></li>
+	          <li class="nav-item"><a href="{{ route('service') }}" class="nav-link">Dịch vụ</a></li>
+	          <li class="nav-item"><a href="{{ route('album') }}" class="nav-link">Album Ảnh</a></li>
+	          <li class="nav-item"><a href="{{ route('product') }}" class="nav-link">Sản phẩm</a></li>
+	          <li class="nav-item"><a href="{{ route('blog') }}" class="nav-link">Bài viết</a></li>
+	          <li class="nav-item"><a href="{{ route('contact') }}" class="nav-link">Liên hệ</a></li>
+              <li class="nav-item dropdown"><a href="{{ route('login') }}" class="nav-link">Đăng nhập</a>
+                <div class="dropdown-content">
+                   <a href="{{ route('signup') }}">Đăng ký</a>
+               </div>
+              </li>
+	        </ul>
 	      </div>
 	    </div>
 	  </nav>
--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', 'Home\HomeController@index')->name('home');
 Route::get('service', 'Home\HomeController@service')->name('service');
 Route::get('produce', 'Home\HomeController@produce')->name('produce');
 Route::get('blog', 'Home\HomeController@blog')->name('blog');
-
 Route::get('contact', 'Home\HomeController@contact')->name('contact');
 Route::get('album', 'Home\HomeController@album')->name('album');
-
-
 Route::get('login', function () {
     return view('home.login');
 })->name('login');
+
