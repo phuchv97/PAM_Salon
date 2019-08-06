683d7d14668b8626e80e9b391d7a9e35dc4acb14
hovanphuc97 <hovanphuc97t10@gmail.com>
2019-08-06 14:05:23 +0700

    no message

--- resources/views/__share/home/header.blade.php
+++ resources/views/__share/home/header.blade.php
@@ -1,25 +1,17 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-	    <div class="container">
-	      <a class="navbar-brand" href="#">PAM.</a>
-	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-	        <span class="oi oi-menu"></span> Menu
-	      </button>
+        <div class="container">
+          <a class="navbar-brand" href="#">PAM.</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="oi oi-menu"></span> Menu
+          </button>
 
-	      <div class="collapse navbar-collapse" id="ftco-nav">
-	        <ul class="navbar-nav ml-auto">
-	          <li class="nav-item active"><a href="{{ route('/') }}" class="nav-link">Trang chủ</a></li>
-	          <li class="nav-item"><a href="{{ route('service') }}" class="nav-link">Dịch vụ</a></li>
-	          <li class="nav-item"><a href="{{ route('album') }}" class="nav-link">Album Ảnh</a></li>
-	          <li class="nav-item"><a href="{{ route('product') }}" class="nav-link">Sản phẩm</a></li>
-	          <li class="nav-item"><a href="{{ route('blog') }}" class="nav-link">Bài viết</a></li>
-	          <li class="nav-item"><a href="{{ route('contact') }}" class="nav-link">Liên hệ</a></li>
-              <li class="nav-item dropdown"><a href="{{ route('login') }}" class="nav-link">Đăng nhập</a>
-                <div class="dropdown-content">
-                   <a href="{{ route('signup') }}">Đăng ký</a>
-               </div>
-              </li>
-	        </ul>
-	      </div>
-	    </div>
-	  </nav>
+          <div class="collapse navbar-collapse" id="ftco-nav">
+            <ul class="navbar-nav ml-auto">
+            @foreach($menus as $menu )
+              <li class="nav-item"><a href="{{route($menu->slug)}}" class="nav-link">{{$menu -> name}}</a></li>
+            @endforeach
+            </ul>
+          </div>
+        </div>
+      </nav>
