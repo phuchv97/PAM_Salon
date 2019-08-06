4900676f5f37ba3ff8f61a5ddda635a5ac714cec
hovanphuc97 <hovanphuc97t10@gmail.com>
2019-08-06 14:02:40 +0700

    no message

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ Route::get('/', 'Home\HomeController@index')->name('home');
 Route::get('service', 'Home\HomeController@service')->name('service');
 Route::get('produce', 'Home\HomeController@produce')->name('produce');
 Route::get('blog', 'Home\HomeController@blog')->name('blog');
+
 Route::get('contact', 'Home\HomeController@contact')->name('contact');
 Route::get('album', 'Home\HomeController@album')->name('album');
+
+
 Route::get('login', function () {
     return view('home.login');
 })->name('login');
-
