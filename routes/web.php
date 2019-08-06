4fbf10c435b1b7b4f2f47c9929a051ffd18ed30d
phuchv97 <53143356+phuchv97@users.noreply.github.com>
2019-08-06 13:53:27 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,3 @@ Route::get('album', 'Home\HomeController@album')->name('album');
 Route::get('login', function () {
     return view('home.login');
 })->name('login');
-Route::get('product-detail', function () {
-    return view('home.product-detail');
-})->name('product-detail');
-Route::get('signup', function () {
-    return view('home.signup');
-})->name('signup');
